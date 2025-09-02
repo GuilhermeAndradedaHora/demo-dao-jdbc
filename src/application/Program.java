10101cbd8c903cd24a10df71990dc7d7be70ac38
Guilherme Hora <guilherme.andradehora@gmail.com>
2025-09-02 15:30:06 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Program {
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
         Seller sl = new Seller(21, "Guilherme", "guilherme@gmail.com", new Date(), 3000.00, dp);
+        SellerDao sellerDao = DaoFactory.cerateSellerDao();
         System.out.println(sl);
 
     }
